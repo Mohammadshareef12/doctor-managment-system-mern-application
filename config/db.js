ffdc4a8812a1ce7a46d32eb239b47ad788285c6f
HASANAPUURAM MOHAMMAD SHAREEF <98573661+Mohammadshareef12@users.noreply.github.com>
2024-06-25 14:45:42 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async() => {
     try {
-    await mongoose.connect('mongodb+srv://learning:1234567890@cluster0.t6fgp1f.mongodb.net/doctor-app');
+    await mongoose.connect('mongodb+srv://hmohammadshareef333:123456789@cluster0.t6fgp1f.mongodb.net/doctor-app');
     console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
     }   
     catch (error) {
